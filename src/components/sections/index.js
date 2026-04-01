32187c4dc05973543c1cb04e51438c9e6e726bd3
SoniPB96 <robinson.laha96@gmail.com>
2026-04-01 19:43:35 +0200

    update

--- src/components/sections/index.js
+++
@@ -1,5 +0,0 @@
-export { default as HeroSection } from './HeroSection';
-export { default as ServicesSection } from './ServicesSection';
-export { default as TrustSection } from './TrustSection';
-export { default as BegleitungSection } from './BegleitungSection';
-export { default as RequestSection } from './RequestSection';
